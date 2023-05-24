1549401e2d833476c157b292d774c756bbfcc3f8
Brian Gadomski <gadomski.brian@gmail.com>
2023-05-24 13:46:34 -0500

    first deploy

---
+++ frontend/src/routes/Foot.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Foot() {
+	
+	return (
+		<div className='fixed-bottom bg-dark text-light text-center' >
+			<h5>&copy;2023 CloudCity LLC</h5>
+			
+			
+		</div>
+	)
+}
